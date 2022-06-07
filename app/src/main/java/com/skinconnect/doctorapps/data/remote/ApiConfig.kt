cdaa8dcd1a8df5aaa472c91bba22161b0433be20
dndyfdlh <dandyfadillah2@gmail.com>
2022-06-07 18:31:58 +0700

    test baseUrl

--- app/src/main/java/com/skinconnect/doctorapps/data/remote/ApiConfig.kt
+++ app/src/main/java/com/skinconnect/doctorapps/data/remote/ApiConfig.kt
@@ -29,7 +29,7 @@ object ApiConfig {
             .build()
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://skinconnect-6e77f4stva-uc.a.run.app/api/v1/")
+            .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .client(client).build()
 
